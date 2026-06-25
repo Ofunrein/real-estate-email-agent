96938d8a3ca4e5a7d848eee3597454c3fc484a2b
Martin O. <Ofunrein@users.noreply.github.com>
2026-06-25 16:09:50 -0500

    fix: restore login auth layout

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,22 @@
 import { redirect } from "next/navigation";
 import type { CSSProperties } from "react";
 
-import { auth, localAuthBypassEnabled } from "@/auth";
+import { auth, hasGoogleAuthProvider, localAuthBypassEnabled, signIn } from "@/auth";
 import { passwordSignInEnabled } from "@/lib/dashboardPasswordAuth";
 import { requestPasswordReset, signInWithPassword } from "./actions";
 
 export const dynamic = "force-dynamic";
+
+function GoogleMark() {
+  return (
+    <svg className="login-google-mark" viewBox="0 0 24 24" aria-hidden="true">
+      <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" />
+      <path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.24 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" />
+      <path fill="#FBBC05" d="M5.84 14.1c-.22-.66-.35-1.36-.35-2.1s.13-1.44.35-2.1V7.06H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.94l3.66-2.84z" />
+      <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.06L5.84 9.9C6.71 7.3 9.14 5.38 12 5.38z" />
+    </svg>
+  );
+}
 
 export default async function LoginPage({
   searchParams,
@@ -23,6 +34,7 @@ export default async function LoginPage({
   const reset = params?.reset || "";
   const devReset = params?.devReset || "";
   const passwordEnabled = passwordSignInEnabled();
+  const googleEnabled = hasGoogleAuthProvider();
 
   return (
     <main className="login-shell">
@@ -93,18 +105,7 @@ export default async function LoginPage({
               />
             </label>
             <label>
-              <span className="login-label-row">
-                Password
-                <button
-                  className="login-forgot-link"
-                  formAction={requestPasswordReset}
-                  type="submit"
-                  disabled={!passwordEnabled}
-                  formNoValidate
-                >
-                  Forgot password?
-                </button>
-              </span>
+              <span>Password</span>
               <input
                 name="password"
                 type="password"
@@ -114,8 +115,33 @@ export default async function LoginPage({
                 disabled={!passwordEnabled}
               />
             </label>
+            <div className="login-forgot-row">
+              <button
+                className="login-forgot-link"
+                formAction={requestPasswordReset}
+                type="submit"
+                disabled={!passwordEnabled}
+                formNoValidate
+              >
+                Forgot password?
+              </button>
+            </div>
             <button className="login-button" type="submit" disabled={!passwordEnabled}>
               Sign in
+            </button>
+          </form>
+
+          <div className="login-divider"><span>or</span></div>
+
+          <form
+            action={async () => {
+              "use server";
+              await signIn("google", { redirectTo: "/" });
+            }}
+          >
+            <button className="login-button login-button-secondary" type="submit" disabled={!googleEnabled}>
+              <GoogleMark />
+              <span>Continue with Google</span>
             </button>
           </form>
         </div>
